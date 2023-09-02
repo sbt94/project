972619676935f92b404462a00894bc1930d9e641
sbt_094 <bentovshahar@gmail.com>
2023-09-02 17:49:23 +0300

    Co-authored-by: bar yadgar <barbar-del@users.noreply.github.com>

--- tables.sql
+++ tables.sql
@@ -13,6 +13,12 @@ DROP TABLE IF EXISTS Logs;
 
 -- Drop the parent table
 DROP TABLE IF EXISTS UserTypes;
+
+USE project
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'sp_InsertNewOrder')
+    DROP PROCEDURE sp_InsertNewOrder;
+
 GO
 
 USE project
@@ -46,7 +52,9 @@ IF OBJECT_ID('tr_Orders_Delete_Log', 'TR') IS NOT NULL
     DROP TRIGGER tr_Orders_Delete_Log;
 GO
 
-
+--------------------------------------------------------------------------------------------   
+--------------------------------------create tables-----------------------------------------
+--------------------------------------------------------------------------------------------   
 
 
 USE project
@@ -80,7 +88,7 @@ CREATE TABLE Employees (
    Email NVARCHAR(100) UNIQUE,
    PhoneNumber NVARCHAR(20),
    HireDate DATE,
-   Salary DECIMAL(10,2),
+   Salary FLOAT, 
    Password NVARCHAR(255),
    UserTypeID INT FOREIGN KEY REFERENCES UserTypes(UserTypeID)
 );
@@ -98,7 +106,7 @@ CREATE TABLE MenuItems (
    MenuItemID INT PRIMARY KEY IDENTITY(1,1),
    Name NVARCHAR(100) NOT NULL,
    Description NVARCHAR(255),
-   Price DECIMAL(10,2) NOT NULL
+   Price FLOAT NOT NULL
 );
 
 -- Reservations
@@ -106,11 +114,11 @@ CREATE TABLE Reservations (
    ReservationID INT PRIMARY KEY IDENTITY(1,1),
    CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID),
    TableID INT FOREIGN KEY REFERENCES Tables(TableID),
-   ReservationTime DATETIME NOT NULL,
-   StartTime DATETIME,
+   StartTime DATETIME NOT NULL,
    EndTime DATETIME
 );
 
+use project
 -- Orders
 CREATE TABLE Orders (
    OrderID INT PRIMARY KEY IDENTITY(1,1),
@@ -139,6 +147,40 @@ CREATE TABLE Logs (
    Action NVARCHAR(100),
    Description NVARCHAR(255)
 );
+GO
+
+--------------------------------------------------------------------------------------------   
+--------------------------------------TRIGERS----------------------------------------------
+--------------------------------------------------------------------------------------------   
+
+CREATE TRIGGER tr_DefaultEndTime -- Trigger to set the default end time to 2 hours after the start time
+ON Reservations
+AFTER INSERT
+AS
+BEGIN
+   UPDATE r
+   SET r.EndTime = DATEADD(HOUR, 2, i.StartTime)
+   FROM Reservations r 
+   INNER JOIN inserted i ON r.ReservationID = i.ReservationID 
+   WHERE r.EndTime IS NULL;
+END;
+GO
+CREATE TRIGGER tr_UpdateEndTime
+ON Reservations
+AFTER UPDATE
+AS
+BEGIN
+   -- Check if the StartTime column was updated
+   IF UPDATE(StartTime)
+   BEGIN
+      UPDATE r
+      SET r.EndTime = DATEADD(HOUR, 2, i.StartTime)
+      FROM Reservations r
+      INNER JOIN inserted i ON r.ReservationID = i.ReservationID
+      INNER JOIN deleted d ON r.ReservationID = d.ReservationID
+      WHERE r.StartTime <> d.StartTime;
+   END
+END;
 GO
 
 CREATE TRIGGER tr_Customers_Insert_Log
@@ -181,7 +223,7 @@ BEGIN
         i.CustomerID, 
         'Customer', 
         'Reservation', 
-        'New reservation for table ' + CAST(i.TableID AS NVARCHAR(10)) + ' at ' + CAST(i.ReservationTime AS NVARCHAR(50)) + '.'
+        'New reservation for table ' + CAST(i.TableID AS NVARCHAR(10)) + ' at ' + CAST(i.StartTime AS NVARCHAR(50)) + '.'
     FROM INSERTED i;
 END;
 GO
@@ -195,7 +237,7 @@ BEGIN
         d.CustomerID, 
         'Customer', 
         'Reservation Deletion', 
-        'Reservation for table ' + CAST(d.TableID AS NVARCHAR(10)) + ' at ' + CAST(d.ReservationTime AS NVARCHAR(50)) + ' deleted.'
+        'Reservation for table ' + CAST(d.TableID AS NVARCHAR(10)) + ' at ' + CAST(d.StartTime AS NVARCHAR(50)) + ' deleted.'
     FROM DELETED d;
 END;
 GO
@@ -319,7 +361,7 @@ INSERT INTO Customers (FirstName, LastName, Email, PhoneNumber, Address, Passwor
 ('Lucas', 'Martin', 'lucas@example.com', '1234567890', '789 Oak St', 'pass789', 2);
 
 -- Insert data for Reservations
-INSERT INTO Reservations (CustomerID, TableID, ReservationTime) VALUES 
+INSERT INTO Reservations (CustomerID, TableID, StartTime) VALUES 
 (1, 1, '2023-09-10 19:00:00'),   -- Note: Using TableID (1 for 101, 2 for 102, 3 for 103)
 (2, 3, '2023-09-11 20:00:00');
 
@@ -345,7 +387,7 @@ GO
 
 use project
 -- Retrieve the tables that were reserved by customers who have also placed an order with a total price greater than a specific value
-SELECT t.TableNumber, r.ReservationTime
+SELECT t.TableNumber, r.StartTime
 FROM Tables t
 JOIN Reservations r ON t.TableID = r.TableID
 WHERE r.CustomerID IN (
@@ -379,3 +421,248 @@ AND (
 )
 WHERE r.TableID IS NULL;
 GO
+
+
+----------------------------------------------------------------
+------------------procedures------------------------------------
+----------------------------------------------------------------
+
+--create a new customer    if the cretion sucseed it will retuen 1 
+USE project
+GO
+
+CREATE PROCEDURE sp_CreateNewUser
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @Email NVARCHAR(100),
+    @PhoneNumber NVARCHAR(20),
+    @Address NVARCHAR(250),
+    @Password NVARCHAR(255),
+    @UserTypeID INT
+AS
+BEGIN
+    -- Initialize return value to 0 (success)
+    DECLARE @ReturnValue INT = 0
+
+    -- Check if the email already exists
+    IF EXISTS (SELECT 1 FROM Customers WHERE Email = @Email)
+    BEGIN
+        SET @ReturnValue = -1  -- Email already exists
+        RETURN @ReturnValue
+    END
+
+    -- Insert the new user
+    INSERT INTO Customers (FirstName, LastName, Email, PhoneNumber, Address, Password, UserTypeID)
+    VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @Address, @Password, @UserTypeID)
+
+    -- Return success
+    RETURN @ReturnValue
+END
+GO
+
+
+
+
+--create a new employee
+USE project
+GO
+
+CREATE PROCEDURE sp_CreateNewEmployee
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @Email NVARCHAR(100),
+    @PhoneNumber NVARCHAR(20),
+    @HireDate DATE,
+    @Salary FLOAT,
+    @Password NVARCHAR(255),
+    @UserTypeID INT
+AS
+BEGIN
+    -- Initialize return value to 0 (success)
+    DECLARE @ReturnValue INT = 1
+
+    -- Check if the email already exists
+    IF EXISTS (SELECT 1 FROM Employees WHERE Email = @Email)
+    BEGIN
+        SET @ReturnValue = 0  -- Email already exists
+        RETURN @ReturnValue
+    END
+
+    -- Insert the new employee
+    INSERT INTO Employees (FirstName, LastName, Email, PhoneNumber, HireDate, Salary, Password, UserTypeID)
+    VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @HireDate, @Salary, @Password, @UserTypeID)
+
+    -- Return success
+    RETURN @ReturnValue
+END
+GO
+
+
+
+
+--add new reservetion
+
+
+
+--add new order
+USE project
+GO
+CREATE PROCEDURE sp_InsertNewOrder
+(
+    @CustomerID INT,
+    @EmployeeID INT,
+    @TotalPrice DECIMAL(10,2),
+    @DeliveryAddress NVARCHAR(250),
+    @Status NVARCHAR(50) = 'Pending', -- Using default value as 'Pending'
+    @Success BIT OUTPUT,             -- Output parameter to indicate success or failure
+    @Message NVARCHAR(255) OUTPUT    -- Output parameter to hold the message
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+    -- Insert new order into Orders table
+        INSERT INTO Orders 
+        (
+            CustomerID,
+            EmployeeID,
+            TotalPrice,
+            DeliveryAddress,
+            Status
+        )
+        VALUES 
+        (
+            @CustomerID,
+            @EmployeeID,
+            @TotalPrice,
+            @DeliveryAddress,
+            @Status
+        )
+        SET @Success = 1
+        SET @Message = 'Order insertion was successful.'
+    END TRY
+    BEGIN CATCH
+        SET @Success = 0
+        SET @Message = ERROR_MESSAGE()
+    END CATCH
+END
+GO
+
+-- DECLARE @Success BIT
+-- DECLARE @ResultMessage NVARCHAR(255)
+
+-- EXEC sp_InsertNewOrder 
+--     @CustomerID = 1, 
+--     @EmployeeID = 1, 
+--     @TotalPrice = 99.99, 
+--     @DeliveryAddress = '123 Main St, City, Country',
+--     @Success = @Success OUTPUT,
+--     @Message = @ResultMessage OUTPUT
+
+-- -- Check the result
+-- SELECT @Success AS Success, @ResultMessage AS Message
+
+
+
+--add new order detail
+
+
+
+--find available tables
+USE project
+GO
+
+CREATE PROCEDURE sp_MakeReservation
+    @CustomerEmail NVARCHAR(100),
+    @StartTime DATETIME,
+    @NumberOfPeople INT,
+    @OutputMessage NVARCHAR(255) OUTPUT
+AS
+BEGIN
+    -- Declare variables
+    DECLARE @CustomerID INT
+    DECLARE @EndTime DATETIME
+    DECLARE @AvailableTableID INT
+
+    -- Initialize variables
+    SET @EndTime = DATEADD(HOUR, 2, @StartTime) -- 2 hours after the start time
+
+    -- Check if the customer already exists
+    SELECT @CustomerID = CustomerID FROM Customers WHERE Email = @CustomerEmail
+
+    -- If customer doesn't exist, exit the procedure
+    IF @CustomerID IS NULL
+    BEGIN
+        SET @OutputMessage = 'Customer does not exist.'
+        RETURN
+    END
+
+    -- Check for an available table
+    SELECT TOP 1 @AvailableTableID = T.TableID
+    FROM Tables T
+    WHERE T.Capacity >= @NumberOfPeople AND T.Status = 'Available'
+    AND NOT EXISTS (
+        SELECT 1 FROM Reservations R
+        WHERE R.TableID = T.TableID
+        AND (
+            (R.StartTime <= @StartTime AND R.EndTime >= @StartTime)
+            OR (R.StartTime <= @EndTime AND R.EndTime >= @EndTime)
+            OR (@StartTime <= R.StartTime AND @EndTime >= R.StartTime)
+        )
+    )
+    ORDER BY T.Capacity ASC
+
+    -- If an available table is found, make the reservation
+    IF @AvailableTableID IS NOT NULL
+    BEGIN
+        INSERT INTO Reservations (CustomerID, TableID, StartTime, EndTime)
+        VALUES (@CustomerID, @AvailableTableID, @StartTime, @EndTime)
+
+        SET @OutputMessage = 'Reservation made successfully.'
+    END
+    ELSE
+    BEGIN
+        SET @OutputMessage = 'No available tables for the given time and capacity.'
+    END
+END
+GO
+
+
+
+
+
+--create a function that look for the accses lvl and return the the user type
+USE project
+GO
+
+CREATE PROCEDURE sp_GetUserTypeByEmail
+    @Email NVARCHAR(100),
+    @UserType NVARCHAR(50) OUTPUT
+AS
+BEGIN
+    -- Initialize the output parameter
+    SET @UserType = NULL
+
+    -- Get the user type based on the email
+    SELECT @UserType = UT.UserType
+    FROM Customers C
+    INNER JOIN UserTypes UT ON C.UserTypeID = UT.UserTypeID
+    WHERE C.Email = @Email
+
+    -- If no user is found with the given email, set UserType to 'Not Found'
+    IF @UserType IS NULL
+    BEGIN
+        SET @UserType = 'Not Found'
+    END
+END
+GO
+
+
+
+
+
+
+----------------------------------------------------------------
+------------------------accsess---------------------------------
+----------------------------------------------------------------
+
